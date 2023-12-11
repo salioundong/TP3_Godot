210077bbc77492b98cec8dc8eebab83da8828c8d
salioundong <2119393@cshawi.ca>
2023-12-11 02:37:35 -0500

    ajout menu principal dans la scene de fin

--- fin_de_jeu.gd
+++ fin_de_jeu.gd
@@ -7,3 +7,9 @@ func _on_button_rejouer_pressed():
 
 func _on_button_quitter_pressed():
 	get_tree().quit()
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://start_menu.tscn")
+
+
